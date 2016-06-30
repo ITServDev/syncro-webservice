64a179144936deb41078ae99636fd7fb9b6f3752
pablotserv <pablo.garcia@itserv.com.br>
2016-06-30 08:59:29 -0300

    Atualizção get run

--- src/controllers/run.controller.js
+++ src/controllers/run.controller.js
@@ -2,14 +2,13 @@ import * as dao from '../dao/run.dao';
 import request from 'superagent';
 
 
-/*function getRun(req, res, next) {
+function getRun(req, res, next) {
   let query = req.query;
   dao.findRun(query, {}).exec((err,data) => {
    return res.json(data);
   })
-}*/
-
-function getRun(req, res, next) {
+}
+/*function getRun(req, res, next) {
  let params = req.params;
  let query ={
     deviceStartDate: params;
@@ -17,8 +16,7 @@ function getRun(req, res, next) {
  dao.findRun(query, {}).exec((err,data) => {
   return res.json(data);
  })
-}
-
+}*/
 function createRun(req, res, next) {
   let params = req.params;
 
