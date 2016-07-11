b14f3c4089a3d73fe4aa62400b41efe08866945e
pablotserv <pablo.garcia@itserv.com.br>
2016-07-11 14:00:19 -0300

    Atualizado

--- src/controllers/run.controller.js
+++ src/controllers/run.controller.js
@@ -2,30 +2,29 @@ import * as dao from '../dao/run.dao';
 import request from 'superagent';
 
 
-//function getRun(req, res, next) {
-//  let query = req.query;
-//  dao.findRun(query, {}).exec((err,data) => {
-//   return res.json(data);
-//  })
-//}
-
-
 function getRun(req, res, next) {
-
+ var usuario = req.params.user;
  var veiculo =req.params.carro;
  let datas = {
    $gte:req.params.$gte,
    $lte:req.params.$lte
  }
- var query = {
-    deviceStartDate:datas,
-    car:veiculo
-  }
+ if(usuario==0){
+   var query = {
+      deviceStartDate:datas,
+      car:veiculo
+    }
+ }else {
+   var query = {
+      deviceStartDate:datas,
+      user:usuario
+    }
+ }
+
  dao.findRun(query, {}).exec((err,data) => {
     return res.json(data);
   })
 }
-
 
 function createRun(req, res, next) {
   let params = req.params;
