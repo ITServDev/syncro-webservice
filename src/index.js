f4370ad1dc3a248e7e5cd07370c333f524aeba91
Pablo Garcia <pablo.garcia@itserv.com.br>
2016-12-02 11:56:48 -0200

    Alteração de porta

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import db from './configs/database.config';
 import cors from './configs/cors.config';
 import { attachRoute } from './configs/routes.config';
 
+
 let app = restify.createServer();
 app.use(restify.bodyParser());
 app.use(restify.queryParser());
@@ -13,16 +14,19 @@ app.use(restify.jsonp());
 app.use(cors());
 
 app.use((req, res, next) => {
-  let user = cache.filter(c => c.key == req.params.token)[0];
+    let user = cache.filter(c => c.key == req.params.token)[0];
 
-  if (user)
-    req.user = user.value
+    if (user)
+        req.user = user.value
 
-   next();
+    next();
 })
 
 attachRoute(app);
 
-app.listen(3000, () => {
-  console.log("Serviço ON-LINE!");
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("\t\t\tSERVICE ON-LINE!");
+    console.log("\t\t\t\tRUNNING...");
+    console.log('');
 })
