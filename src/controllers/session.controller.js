cb1341a4dc37db4065e8c8596e5a28d5d91383c0
Pablo Garcia <pablo.garcia@itserv.com.br>
2016-12-02 11:59:49 -0200

    alteração de porta

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -4,30 +4,30 @@ import uId from 'uid';
 import { cache } from '../configs/constants.config';
 
 function getSession(req, res, next) {
-  return res.json({controller: 'getSession'});
+    return res.json({ controller: 'getSession' });
 }
 
 function getSessionUser(req, res, next) {
-  let user = cache.filter(c => c.key == req.params.token)[0];
-  return res.json(user);
+    let user = cache.filter(c => c.key == req.params.token)[0];
+    return res.json(user);
 }
 
 function createSession(req, res, next) {
-  dao.findOneUser(req.params, {})
-    .exec((err, data) => {
-      let key;
+    dao.findOneUser(req.params, {})
+        .exec((err, data) => {
+            let key;
 
-      if (!err) {
-        key = uId(15);
-        cache.push({key: key, value: data});
-      }
+            if (!err) {
+                key = uId(15);
+                cache.push({ key: key, value: data });
+            }
 
-      return res.json(!key ? err : {token: key});
-    })
+            return res.json(!key ? err : { token: key });
+        })
 }
 
 export default {
-  get: getSession,
-  getUser: getSessionUser,
-  post: createSession
+    get: getSession,
+    getUser: getSessionUser,
+    post: createSession
 }
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,46 +2,48 @@ import * as dao from '../dao/user.dao';
 import request from 'superagent';
 
 function createUsersOnMongo(req, res, next) {
-  request
-    .get('http://www.itserv.com.br/ITServ/api/Login')
-    .set('Accept', 'application/json')
-    .end((err, response) => {
-      if (err) return;
-        let users = JSON.parse(response.text);
-
-        users.forEach(user => {
-          if (!user.active) {
-            dao.deleteUser({cpf: user.cpf})
-              .exec((err, data) => {
-                if (err) return res.json(err);
-                return res.json("User deactiveted", user.name)
-              });
-          } else {
-            dao.createUser(user)
-            .save((err, data) => {
-              if (err) return res.json(err);
-              return res.json("User updated", user.name)
-            })
-          }
+    request
+        .get('https://restfull-pablo440.c9users.io/login')
+        .set('Accept', 'application/json')
+        .end((err, response) => {
+            if (err) return;
+            let users = JSON.parse(response.text);
+            //http://www.itserv.com.br/ITServ/api/Login
+            //http://localhost:3000/Login
+            //https://restfull-pablo440.c9users.io/login
+            users.forEach(user => {
+                if (!user.active) {
+                    dao.deleteUser({ cpf: user.cpf })
+                        .exec((err, data) => {
+                            if (err) return res.json(err);
+                            return res.json("User deactiveted", user.name)
+                        });
+                } else {
+                    dao.createUser(user)
+                        .save((err, data) => {
+                            if (err) return res.json(err);
+                            return res.json("User updated", user.name)
+                        })
+                }
+            });
         });
-    });
 }
 
 function updateUser(req, res, next) {
-     var query = {
-     _id: req.params.id
-   }
-   var mod = {
-     "$set": { password: req.params.password }
-   }
+    var query = {
+        _id: req.params.id
+    }
+    var mod = {
+        "$set": { password: req.params.password }
+    }
 
-  dao.findOneUserAndUpdate(query, mod)
-     .exec((err, data) => {
-         return res.json(err ? err : data);
-      })
+    dao.findOneUserAndUpdate(query, mod)
+        .exec((err, data) => {
+            return res.json(err ? err : data);
+        })
 }
 
 export default {
-  post: createUsersOnMongo,
-  put: updateUser
+    post: createUsersOnMongo,
+    put: updateUser
 }
