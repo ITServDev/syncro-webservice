6d0304027a21d878fb25eb28cab4b5a94a6213c8
pabloitserv <pablo.garcia@itserv.com.br>
2017-05-10 08:22:02 -0300

    Set Cors

--- src/configs/constants.config.js
+++ src/configs/constants.config.js
@@ -1,4 +1,4 @@
-export const DATABASE_URL = 'mongodb://localhost/dblocal';
+export const DATABASE_URL = 'mongodb://localhost/syncro-webservice';
 
 export const SESSION_URL = '/session';
 export const SESSION_USER_URL = '/session/user';
--- src/configs/cors.config.js
+++ src/configs/cors.config.js
@@ -2,6 +2,7 @@ export const cors = () => (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Request-With, content-type, Authorization');
+  res.setHeader('Content-Type', 'application/json');
   return next();
 };
 
